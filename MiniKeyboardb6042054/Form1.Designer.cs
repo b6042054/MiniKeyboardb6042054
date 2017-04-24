72a0c147f453827bd08c2e46d7bb9d1d99a90fc4
Abigail Hunter <b6042054@my.shu.ac.uk>
2017-04-24 12:20:24 +0100

    Section 1
    Buttons, text boxes and colours added

--- MiniKeyboardb6042054/Form1.Designer.cs
+++ MiniKeyboardb6042054/Form1.Designer.cs
@@ -28,12 +28,285 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.ModeButton = new System.Windows.Forms.Button();
+            this.NextButton = new System.Windows.Forms.Button();
+            this.PrevButton = new System.Windows.Forms.Button();
+            this.SendPredictedButton = new System.Windows.Forms.Button();
+            this.Char7 = new System.Windows.Forms.Button();
+            this.Char8 = new System.Windows.Forms.Button();
+            this.Char9 = new System.Windows.Forms.Button();
+            this.Char4 = new System.Windows.Forms.Button();
+            this.Char5 = new System.Windows.Forms.Button();
+            this.Char6 = new System.Windows.Forms.Button();
+            this.Char1 = new System.Windows.Forms.Button();
+            this.Char2 = new System.Windows.Forms.Button();
+            this.Char3 = new System.Windows.Forms.Button();
+            this.Char10 = new System.Windows.Forms.Button();
+            this.Char0 = new System.Windows.Forms.Button();
+            this.Char11 = new System.Windows.Forms.Button();
+            this.EnterButton = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(22, 37);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(725, 261);
+            this.textBox1.TabIndex = 0;
+            // 
+            // ModeButton
+            // 
+            this.ModeButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ModeButton.Location = new System.Drawing.Point(242, 384);
+            this.ModeButton.Name = "ModeButton";
+            this.ModeButton.Size = new System.Drawing.Size(60, 60);
+            this.ModeButton.TabIndex = 1;
+            this.ModeButton.Text = "Mode";
+            this.ModeButton.UseVisualStyleBackColor = false;
+            // 
+            // NextButton
+            // 
+            this.NextButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.NextButton.Location = new System.Drawing.Point(318, 384);
+            this.NextButton.Name = "NextButton";
+            this.NextButton.Size = new System.Drawing.Size(60, 60);
+            this.NextButton.TabIndex = 2;
+            this.NextButton.Text = "Next";
+            this.NextButton.UseVisualStyleBackColor = false;
+            // 
+            // PrevButton
+            // 
+            this.PrevButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.PrevButton.Location = new System.Drawing.Point(395, 384);
+            this.PrevButton.Name = "PrevButton";
+            this.PrevButton.Size = new System.Drawing.Size(60, 60);
+            this.PrevButton.TabIndex = 3;
+            this.PrevButton.Text = "Prev";
+            this.PrevButton.UseVisualStyleBackColor = false;
+            // 
+            // SendPredictedButton
+            // 
+            this.SendPredictedButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.SendPredictedButton.Location = new System.Drawing.Point(473, 384);
+            this.SendPredictedButton.Name = "SendPredictedButton";
+            this.SendPredictedButton.Size = new System.Drawing.Size(60, 60);
+            this.SendPredictedButton.TabIndex = 4;
+            this.SendPredictedButton.Text = "Send Predicted";
+            this.SendPredictedButton.UseVisualStyleBackColor = false;
+            // 
+            // Char7
+            // 
+            this.Char7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Char7.Location = new System.Drawing.Point(242, 460);
+            this.Char7.Name = "Char7";
+            this.Char7.Size = new System.Drawing.Size(60, 60);
+            this.Char7.TabIndex = 5;
+            this.Char7.Text = "     7        . , \"";
+            this.Char7.UseVisualStyleBackColor = false;
+            // 
+            // Char8
+            // 
+            this.Char8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Char8.Location = new System.Drawing.Point(318, 460);
+            this.Char8.Name = "Char8";
+            this.Char8.Size = new System.Drawing.Size(60, 60);
+            this.Char8.TabIndex = 6;
+            this.Char8.Text = "      8       A B C";
+            this.Char8.UseVisualStyleBackColor = false;
+            // 
+            // Char9
+            // 
+            this.Char9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Char9.Location = new System.Drawing.Point(395, 460);
+            this.Char9.Name = "Char9";
+            this.Char9.Size = new System.Drawing.Size(60, 60);
+            this.Char9.TabIndex = 7;
+            this.Char9.Text = "      9       D E F";
+            this.Char9.UseVisualStyleBackColor = false;
+            // 
+            // Char4
+            // 
+            this.Char4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Char4.Location = new System.Drawing.Point(242, 535);
+            this.Char4.Name = "Char4";
+            this.Char4.Size = new System.Drawing.Size(60, 60);
+            this.Char4.TabIndex = 8;
+            this.Char4.Text = "      4       G H I";
+            this.Char4.UseVisualStyleBackColor = false;
+            // 
+            // Char5
+            // 
+            this.Char5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Char5.Location = new System.Drawing.Point(318, 535);
+            this.Char5.Name = "Char5";
+            this.Char5.Size = new System.Drawing.Size(60, 60);
+            this.Char5.TabIndex = 9;
+            this.Char5.Text = "      5       J K L";
+            this.Char5.UseVisualStyleBackColor = false;
+            // 
+            // Char6
+            // 
+            this.Char6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Char6.Location = new System.Drawing.Point(395, 535);
+            this.Char6.Name = "Char6";
+            this.Char6.Size = new System.Drawing.Size(60, 60);
+            this.Char6.TabIndex = 10;
+            this.Char6.Text = "      6       M N O";
+            this.Char6.UseVisualStyleBackColor = false;
+            // 
+            // Char1
+            // 
+            this.Char1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Char1.Location = new System.Drawing.Point(242, 610);
+            this.Char1.Name = "Char1";
+            this.Char1.Size = new System.Drawing.Size(60, 60);
+            this.Char1.TabIndex = 11;
+            this.Char1.Text = "     1      P Q R S";
+            this.Char1.UseVisualStyleBackColor = false;
+            // 
+            // Char2
+            // 
+            this.Char2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Char2.Location = new System.Drawing.Point(318, 610);
+            this.Char2.Name = "Char2";
+            this.Char2.Size = new System.Drawing.Size(60, 60);
+            this.Char2.TabIndex = 12;
+            this.Char2.Text = "      2       T U V";
+            this.Char2.UseVisualStyleBackColor = false;
+            // 
+            // Char3
+            // 
+            this.Char3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Char3.Location = new System.Drawing.Point(395, 610);
+            this.Char3.Name = "Char3";
+            this.Char3.Size = new System.Drawing.Size(60, 60);
+            this.Char3.TabIndex = 13;
+            this.Char3.Text = "     3      W X Y Z";
+            this.Char3.UseVisualStyleBackColor = false;
+            // 
+            // Char10
+            // 
+            this.Char10.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Char10.Location = new System.Drawing.Point(242, 685);
+            this.Char10.Name = "Char10";
+            this.Char10.Size = new System.Drawing.Size(60, 60);
+            this.Char10.TabIndex = 14;
+            this.Char10.Text = "* - _";
+            this.Char10.UseVisualStyleBackColor = false;
+            // 
+            // Char0
+            // 
+            this.Char0.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.Char0.Location = new System.Drawing.Point(318, 685);
+            this.Char0.Name = "Char0";
+            this.Char0.Size = new System.Drawing.Size(60, 60);
+            this.Char0.TabIndex = 15;
+            this.Char0.Text = "    000      [ ___ ]";
+            this.Char0.UseVisualStyleBackColor = false;
+            // 
+            // Char11
+            // 
+            this.Char11.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Char11.Location = new System.Drawing.Point(395, 685);
+            this.Char11.Name = "Char11";
+            this.Char11.Size = new System.Drawing.Size(60, 60);
+            this.Char11.TabIndex = 16;
+            this.Char11.Text = "     #      0 0 0";
+            this.Char11.UseVisualStyleBackColor = false;
+            // 
+            // EnterButton
+            // 
+            this.EnterButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.EnterButton.Location = new System.Drawing.Point(473, 610);
+            this.EnterButton.Name = "EnterButton";
+            this.EnterButton.Size = new System.Drawing.Size(60, 135);
+            this.EnterButton.TabIndex = 17;
+            this.EnterButton.Text = "Enter";
+            this.EnterButton.UseVisualStyleBackColor = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(245, 333);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(182, 28);
+            this.richTextBox1.TabIndex = 18;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(450, 334);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(82, 20);
+            this.textBox2.TabIndex = 19;
+            // 
+            // vScrollBar1
+            // 
+            this.vScrollBar1.Location = new System.Drawing.Point(726, 37);
+            this.vScrollBar1.Name = "vScrollBar1";
+            this.vScrollBar1.Size = new System.Drawing.Size(20, 260);
+            this.vScrollBar1.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(772, 778);
+            this.Controls.Add(this.vScrollBar1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.EnterButton);
+            this.Controls.Add(this.Char11);
+            this.Controls.Add(this.Char0);
+            this.Controls.Add(this.Char10);
+            this.Controls.Add(this.Char3);
+            this.Controls.Add(this.Char2);
+            this.Controls.Add(this.Char1);
+            this.Controls.Add(this.Char6);
+            this.Controls.Add(this.Char5);
+            this.Controls.Add(this.Char4);
+            this.Controls.Add(this.Char9);
+            this.Controls.Add(this.Char8);
+            this.Controls.Add(this.Char7);
+            this.Controls.Add(this.SendPredictedButton);
+            this.Controls.Add(this.PrevButton);
+            this.Controls.Add(this.NextButton);
+            this.Controls.Add(this.ModeButton);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button ModeButton;
+        private System.Windows.Forms.Button NextButton;
+        private System.Windows.Forms.Button PrevButton;
+        private System.Windows.Forms.Button SendPredictedButton;
+        private System.Windows.Forms.Button Char7;
+        private System.Windows.Forms.Button Char8;
+        private System.Windows.Forms.Button Char9;
+        private System.Windows.Forms.Button Char4;
+        private System.Windows.Forms.Button Char5;
+        private System.Windows.Forms.Button Char6;
+        private System.Windows.Forms.Button Char1;
+        private System.Windows.Forms.Button Char2;
+        private System.Windows.Forms.Button Char3;
+        private System.Windows.Forms.Button Char10;
+        private System.Windows.Forms.Button Char0;
+        private System.Windows.Forms.Button Char11;
+        private System.Windows.Forms.Button EnterButton;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.VScrollBar vScrollBar1;
     }
 }
 
