c56fcb0cf67e5df9d4f71045c666a500db16f984
Abigail Hunter <b6042054@my.shu.ac.uk>
2017-04-27 19:13:27 +0100

    s2
    save as

--- MiniKeyboardb6042054/MiniKeyboard.cs
+++ MiniKeyboardb6042054/MiniKeyboard.cs
@@ -354,22 +354,52 @@ namespace MiniKeyboardb6042054
 
         private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            if (CFile_Name == "")
-            {
-                saveFileDialog1.InitialDirectory = "C:\\";
+         //   if (CFile_Name == "")
+           // {
+             //   saveFileDialog1.InitialDirectory = "C:\\";
 
-                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+             //   if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+              //  {
+               //     StreamWriter OutputStream = File.CreateText(saveFileDialog1.FileName);
+               //     OutputStream.Write(textBox1.Text);
+               //     CFile_Name = saveFileDialog1.FileName;
+               //     OutputStream.Close();
+             //   }
+            }
+           // else
+            {
+            //    saveToolStripMenuItem_Click(sender, e);
+            }
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Stream myStream = null;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
                 {
-                    StreamWriter OutputStream = File.CreateText(saveFileDialog1.FileName);
-                    OutputStream.Write(textBox1.Text);
-                    CFile_Name = saveFileDialog1.FileName;
-                    OutputStream.Close();
+                    if ((myStream = openFileDialog1.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            // Insert code to read the stream here.
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
                 }
             }
-            else
-            {
-                saveToolStripMenuItem_Click(sender, e);
-            }
+
         }
     }
 }
