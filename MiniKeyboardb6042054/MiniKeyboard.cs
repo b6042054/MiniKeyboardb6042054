aea916c866dc21d3b6b391eecb2f2903f9bfde55
Abigail Hunter <b6042054@my.shu.ac.uk>
2017-04-27 16:01:34 +0100

    s2
    all options done

--- MiniKeyboardb6042054/MiniKeyboard.cs
+++ MiniKeyboardb6042054/MiniKeyboard.cs
@@ -83,125 +83,147 @@ namespace MiniKeyboardb6042054
 
         private void Char9_Click(object sender, EventArgs e)
         {
-            click++;
 
-            switch (click)
+            if (ModeStatus.Text == "Multi-Press")
             {
-                case 1: richTextBox1.Text = "d"; break;
-                case 2: richTextBox1.Text = "e"; break;
-                case 3: richTextBox1.Text = "f"; break;
-                case 4: richTextBox1.Text = "9"; break;
-                case 5: richTextBox1.Text = "D"; break;
-                case 6: richTextBox1.Text = "E"; break;
-                case 7: richTextBox1.Text = "F"; break;
-                case 8: click = 1; richTextBox1.Text = "d"; break;
-            }
+                click++;
 
+                switch (click)
+                {
+                    case 1: richTextBox1.Text = "d"; break;
+                    case 2: richTextBox1.Text = "e"; break;
+                    case 3: richTextBox1.Text = "f"; break;
+                    case 4: richTextBox1.Text = "9"; break;
+                    case 5: richTextBox1.Text = "D"; break;
+                    case 6: richTextBox1.Text = "E"; break;
+                    case 7: richTextBox1.Text = "F"; break;
+                    case 8: click = 1; richTextBox1.Text = "d"; break;
+                }
+            }
         }
 
         private void Char4_Click(object sender, EventArgs e)
         {
-            click++;
 
-            switch (click)
+            if (ModeStatus.Text == "Multi-Press")
             {
-                case 1: richTextBox1.Text = "g"; break;
-                case 2: richTextBox1.Text = "h"; break;
-                case 3: richTextBox1.Text = "i"; break;
-                case 4: richTextBox1.Text = "4"; break;
-                case 5: richTextBox1.Text = "G"; break;
-                case 6: richTextBox1.Text = "H"; break;
-                case 7: richTextBox1.Text = "I"; break;
-                case 8: click = 1; richTextBox1.Text = "g"; break;
+                click++;
+
+                switch (click)
+                {
+                    case 1: richTextBox1.Text = "g"; break;
+                    case 2: richTextBox1.Text = "h"; break;
+                    case 3: richTextBox1.Text = "i"; break;
+                    case 4: richTextBox1.Text = "4"; break;
+                    case 5: richTextBox1.Text = "G"; break;
+                    case 6: richTextBox1.Text = "H"; break;
+                    case 7: richTextBox1.Text = "I"; break;
+                    case 8: click = 1; richTextBox1.Text = "g"; break;
+                }
             }
         }
 
         private void Char5_Click(object sender, EventArgs e)
         {
-            click++;
-
-            switch (click)
+            if (ModeStatus.Text == "Multi-Press")
             {
-                case 1: richTextBox1.Text = "j"; break;
-                case 2: richTextBox1.Text = "k"; break;
-                case 3: richTextBox1.Text = "l"; break;
-                case 4: richTextBox1.Text = "5"; break;
-                case 5: richTextBox1.Text = "J"; break;
-                case 6: richTextBox1.Text = "K"; break;
-                case 7: richTextBox1.Text = "L"; break;
-                case 8: click = 1; richTextBox1.Text = "j"; break;
+                click++;
+
+                switch (click)
+                {
+                    case 1: richTextBox1.Text = "j"; break;
+                    case 2: richTextBox1.Text = "k"; break;
+                    case 3: richTextBox1.Text = "l"; break;
+                    case 4: richTextBox1.Text = "5"; break;
+                    case 5: richTextBox1.Text = "J"; break;
+                    case 6: richTextBox1.Text = "K"; break;
+                    case 7: richTextBox1.Text = "L"; break;
+                    case 8: click = 1; richTextBox1.Text = "j"; break;
+                }
             }
         }
 
         private void Char6_Click(object sender, EventArgs e)
         {
-            click++;
-
-            switch (click)
+            if (ModeStatus.Text == "Multi-Press")
             {
-                case 1: richTextBox1.Text = "m"; break;
-                case 2: richTextBox1.Text = "n"; break;
-                case 3: richTextBox1.Text = "o"; break;
-                case 4: richTextBox1.Text = "6"; break;
-                case 5: richTextBox1.Text = "M"; break;
-                case 6: richTextBox1.Text = "N"; break;
-                case 7: richTextBox1.Text = "O"; break;
-                case 8: click = 1; richTextBox1.Text = "m"; break;
+                click++;
+
+                switch (click)
+                {
+                    case 1: richTextBox1.Text = "m"; break;
+                    case 2: richTextBox1.Text = "n"; break;
+                    case 3: richTextBox1.Text = "o"; break;
+                    case 4: richTextBox1.Text = "6"; break;
+                    case 5: richTextBox1.Text = "M"; break;
+                    case 6: richTextBox1.Text = "N"; break;
+                    case 7: richTextBox1.Text = "O"; break;
+                    case 8: click = 1; richTextBox1.Text = "m"; break;
+                }
             }
         }
 
         private void Char1_Click(object sender, EventArgs e)
         {
-            click++;
-
-            switch (click)
+            if (ModeStatus.Text == "Multi-Press")
             {
-                case 1: richTextBox1.Text = "p"; break;
-                case 2: richTextBox1.Text = "q"; break;
-                case 3: richTextBox1.Text = "r"; break;
-                case 4: richTextBox1.Text = "s"; break;
-                case 5: richTextBox1.Text = "1"; break;
-                case 6: richTextBox1.Text = "P"; break;
-                case 7: richTextBox1.Text = "Q"; break;
-                case 8: richTextBox1.Text = "R"; break;
-                case 9: richTextBox1.Text = "S"; break;
-                case 10: click = 1; richTextBox1.Text = "p"; break;
+                click++;
+
+                switch (click)
+                {
+                    case 1: richTextBox1.Text = "p"; break;
+                    case 2: richTextBox1.Text = "q"; break;
+                    case 3: richTextBox1.Text = "r"; break;
+                    case 4: richTextBox1.Text = "s"; break;
+                    case 5: richTextBox1.Text = "1"; break;
+                    case 6: richTextBox1.Text = "P"; break;
+                    case 7: richTextBox1.Text = "Q"; break;
+                    case 8: richTextBox1.Text = "R"; break;
+                    case 9: richTextBox1.Text = "S"; break;
+                    case 10: click = 1; richTextBox1.Text = "p"; break;
+                }
             }
         }
 
         private void Char2_Click(object sender, EventArgs e)
         {
-            click++;
-
-            switch (click)
+            if (ModeStatus.Text == "Multi-Press")
             {
-                case 1: richTextBox1.Text = "t"; break;
-                case 2: richTextBox1.Text = "u"; break;
-                case 3: richTextBox1.Text = "v"; break;
-                case 4: richTextBox1.Text = "2"; break;
-                case 5: richTextBox1.Text = "T"; break;
-                case 6: richTextBox1.Text = "U"; break;
-                case 7: richTextBox1.Text = "V"; break;
-                case 8: click = 1; richTextBox1.Text = "t"; break;
+                click++;
+
+                switch (click)
+                {
+                    case 1: richTextBox1.Text = "t"; break;
+                    case 2: richTextBox1.Text = "u"; break;
+                    case 3: richTextBox1.Text = "v"; break;
+                    case 4: richTextBox1.Text = "2"; break;
+                    case 5: richTextBox1.Text = "T"; break;
+                    case 6: richTextBox1.Text = "U"; break;
+                    case 7: richTextBox1.Text = "V"; break;
+                    case 8: click = 1; richTextBox1.Text = "t"; break;
+                }
             }
         }
 
         private void Char3_Click(object sender, EventArgs e)
         {
-            click++;
-
-            switch (click)
+            if (ModeStatus.Text == "Multi-Press")
             {
-                case 1: richTextBox1.Text = "w"; break;
-                case 2: richTextBox1.Text = "x"; break;
-                case 3: richTextBox1.Text = "y"; break;
-                case 4: richTextBox1.Text = "z"; break;
-                case 5: richTextBox1.Text = "3"; break;
-                case 6: richTextBox1.Text = "W"; break;
-                case 7: richTextBox1.Text = "X"; break;
-                case 8: richTextBox1.Text = "Y"; break;
-                case 9: richTextBox1.Text = "Z"; break;
-                case 10: click = 1; richTextBox1.Text = "w"; break;
+                click++;
+
+                switch (click)
+                {
+                    case 1: richTextBox1.Text = "w"; break;
+                    case 2: richTextBox1.Text = "x"; break;
+                    case 3: richTextBox1.Text = "y"; break;
+                    case 4: richTextBox1.Text = "z"; break;
+                    case 5: richTextBox1.Text = "3"; break;
+                    case 6: richTextBox1.Text = "W"; break;
+                    case 7: richTextBox1.Text = "X"; break;
+                    case 8: richTextBox1.Text = "Y"; break;
+                    case 9: richTextBox1.Text = "Z"; break;
+                    case 10: click = 1; richTextBox1.Text = "w"; break;
+                }
             }
         }
 
